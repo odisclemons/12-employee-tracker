8432d81282b12ee24212f5ca1dc3446b16a41e20
Odis Clemons <odis.clemons@gmail.com>
2022-05-26 13:36:11 -0400

    removed my models and replaced with schema.sql

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,14 @@
-const Sequelize = require("sequelize");
 const { DB_USER, DB_PASSWORD } = process.env;
 const mysql = require("mysql2");
 
 const dbName = "employees";
 
-function init() {
-  try {
-    mysql.createConnection({
-      host: "localhost",
-      user: DB_USER,
-      password: DB_PASSWORD,
-      database: "employees",
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-init();
-
-let db = new Sequelize("employees", DB_USER, DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
-
-module.exports = db;
+module.exports = mysql.createConnection(
+  {
+    host: "localhost",
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: "employees",
+  },
+  console.log(`Connected to "${dbName}" db.`)
+);
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,25 @@
 DROP DATABASE IF EXISTS employees;
+
 CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
