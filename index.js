f52bcfd00b5659dc7993c05f91260e4a62a9037a
Odis Clemons <odis.clemons@gmail.com>
2022-05-27 16:34:55 -0400

    added switch statement to get choice from user

--- index.js
+++ index.js
@@ -9,16 +9,39 @@ const questions = [
     name: "choose",
     message: "What would you like to do?",
     choices: [
+      { name: "View Departments", value: "viewDept" },
+      { name: "View Roles", value: "viewRole" },
+      { name: "View Employees", value: "viewEmp" },
+      new inq.Separator(),
       { name: "Add Department", value: "addDept" },
       { name: "Add Role", value: "addRole" },
       { name: "Add Employee", value: "addEmp" },
+      new inq.Separator(),
+      { name: "Quit", value: "quit" },
     ],
   },
 ];
 
-inq.prompt(questions).then(async (answers) => {
-  console.log(q.getDeps);
-  let deps = await db.query(q.getDeps);
-  console.log(deps[0]);
-  process.exit();
-});
+async function init() {
+  let choice = await inq.prompt(questions);
+
+  switch (choice) {
+    case "viewDept":
+      break;
+    case "viewRole":
+      break;
+    case "viewEmp":
+      break;
+    case "addDept":
+      break;
+    case "addRole":
+      break;
+    case "addEmp":
+      break;
+    case "quit":
+      console.log("Peace!");
+      process.exit();
+      break;
+  }
+}
+init();
